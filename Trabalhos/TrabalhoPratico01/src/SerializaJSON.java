0e4f54d624d21ea3680b000832035e00d1f1d457
Lizandra <48724855+lizandra18@users.noreply.github.com>
2022-09-26 09:35:40 -0300

    Update SerializaJSON.java

--- Trabalhos/TrabalhoPratico01/src/SerializaJSON.java
+++ Trabalhos/TrabalhoPratico01/src/SerializaJSON.java
@@ -1,5 +1,4 @@
-/* 2. Crie uma classe Java para cadastrar dados relacionados à entidade
-definida na questão 1. A classe deve receber dados via teclado e os
+/* 2. Crie uma classe Java para cadastrar dados relacionados à entidade definida na questão 1. A classe deve receber dados via teclado e os
 salvar em um arquivo JSON. */
 
 public class SerializaJSON {
